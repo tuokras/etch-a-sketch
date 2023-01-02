3a95860cf45432d4fbab2085bfaa70dfa5f04660
tuokras <tuokras@gmail.com>
2023-01-02 18:59:14 +0200

    Add ui for grid size

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
-let column = 0;
-let row = 0;
-for (let i = 0; i < 16; i++) {
-    var div0 = document.createElement("div");
-    //div0.textContent = "div";
-    div0.setAttribute("id", "column-" + column)
-    document.querySelector("#container").appendChild(div0);
+function createGrid() {
+    let column = 0;
+    let row = 0;
+    for (let i = 0; i < gridSize; i++) {
+        var div0 = document.createElement("div");
+        //div0.textContent = "div";
+        div0.setAttribute("id", "column-" + column)
+        document.querySelector("#container").appendChild(div0);
 
-    for (let i = 0; i < 16; i++) {
-        var div1 = document.createElement("div");
-        div1.textContent = "x";
-        div1.setAttribute("id", "row-" + row)
-        div1.setAttribute("class", "box");
-        document.querySelector("#column-" + column).appendChild(div1);
-        row++;
+        for (let i = 0; i < gridSize; i++) {
+            var div1 = document.createElement("div");
+            div1.setAttribute("id", "row-" + row)
+            div1.setAttribute("class", "box");
+            document.querySelector("#column-" + column).appendChild(div1);
+            row++;
+        }
+        column++;
+        row = 0;
     }
-    column++;
-    row = 0;
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
+        box.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = "orange";
+            /* setTimeout(() => {
+                event.target.style.backgroundColor = "";
+            }, 1000); */
+        })
+    })
 }
 
-let boxes = document.querySelectorAll(".box");
-boxes.forEach((box) => {
-    box.addEventListener("mouseover", (event) => {
-        event.target.style.backgroundColor = "orange";
-        setTimeout(() => {
-            event.target.style.backgroundColor = "";
-        }, 1000);
+function clearGrid() {
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
+        box.remove();
     })
+}
+
+
+let gridSize = 16;
+document.querySelector("button").addEventListener("click", (event) => {
+    gridSize = prompt("Enter grid size", gridSize);
+    clearGrid();
+    createGrid();
+
 })
 
+
+
+createGrid();
